5e8acee2fa70ab6787004a2fc0b9f589e3c94da0
Sebastian Mora <seba@MacBook-de-Sebastian.local>
2021-06-19 12:08:35 -0400

    creacion de la seccion de card

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+$ ()//tooltip
+$(function () {
+    var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus()
+})
+});
