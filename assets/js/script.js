bf9867deb5f358731c8eb01f057d7cba895aa73f
Sebastian Mora <seba@MacBook-de-Sebastian.local>
2021-06-19 22:32:54 -0400

    se corrige el funcionamiento del nav bar y se implmenta un trigger en contactos

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,13 @@ $(function () {
     myInput.focus()
 })
 });
+
+//enviar a correo
+$(document).ready(function() {
+    $("#success-alert").hide();
+    $("#enviarCorreo").click(function showAlert() {
+      $("#success-alert").fadeTo(2000, 500).slideUp(500, function() {
+        $("#success-alert").slideUp(500);
+      });
+    });
+  });
